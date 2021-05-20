11c3a605cccca8faade4d74a567da1860b21ddd3
Megan Lukes@gmail.com <meglukes@gmail.com>
2021-05-20 16:44:40 -0400

    Changes

--- public/main.js
+++ public/main.js
@@ -1,7 +1,42 @@
-function main() {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
+/*let team1Count = 0
+let team2Count = 0
+function changeTeam1Score(event) {
+  const clickedButton = event.target
+  if (clickedButton.classList.contains('add')) {
+    team1Count = team1Count + 1
   }
+  if (clickedButton.classList.contains('subtract') && team1Count > 0) {
+    team1Count = team1Count - 1
+  }
+  const score = document.querySelector('team1 h3')
+  score.textContent = `${team1Count}`
+}
+*/
+//Update Team 1 Name
+function updateTeam1Name(evt) {
+  const teamName = document.querySelector('.team1 h2')
+  teamName.textContent = evt.target.value
+}
+//Update Team 2 Name
+function updateTeam2Name(evt) {
+  const teamName = document.querySelector('.team2 h2')
+  teamName.textContent = evt.target.value
+}
+
+function main() {
+  document
+    .querySelectorAll('.team1 i')
+    .addEventListener('click', changeTeam1Score)
+
+  //team 1 name
+  document
+    .querySelector('.team1 input')
+    .addEventListener('input', updateTeam1Name)
+  //Team 2 name
+  document
+    .querySelector('.team2 input')
+    .addEventListener('input', updateTeam2Name)
+  document.querySelector('.team1 ')
 }
 
 document.addEventListener('DOMContentLoaded', main)
